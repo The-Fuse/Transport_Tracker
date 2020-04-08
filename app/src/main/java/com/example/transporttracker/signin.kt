cf06f66a53ab01e2cc3514559e50770b2f817337
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-08 23:36:40 +0530

    Registration for new users to firebase

---
+++ app/src/main/java/com/example/transporttracker/signin.kt
@@ -0,0 +1,45 @@
+package com.example.transporttracker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_signin.*
+
+class signin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signin)
+        val auth = FirebaseAuth.getInstance()
+        signup.setOnClickListener {
+            performregister()
+        }
+    }
+    private fun performregister(){
+        val email=email.text.toString()
+        val password= password.text.toString()
+        val confirmpassword=confpassword.text.toString()
+
+        if(password===confirmpassword){
+            if (email.isEmpty() || password.isEmpty()){
+                Toast.makeText(this,"PLease fill up the credentials!",Toast.LENGTH_SHORT).show()
+                return
+            }
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful){
+                        Toast.makeText(this,"Please try again",Toast.LENGTH_SHORT).show()
+                        return@addOnCompleteListener
+                    }else{
+                        Toast.makeText(this,"Account created successfully!!",Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this,Home::class.java)
+                        intent.flags=
+                            Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    }
+                }
+        }
+    }
+}
