3c17e8fbaff4b5ddf6c2d9cbe837a53eebf94fc8
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-08 17:20:36 +0530

    Connected login to firebase.

--- app/src/main/java/com/example/transporttracker/Home.kt
+++ app/src/main/java/com/example/transporttracker/Home.kt
@@ -7,24 +7,23 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_home2.*
 
 class Home : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home2)
-    }
-    private fun loginToFirebase() {
-        val email = getString(findViewById<null>(R.id.email))
-        val password = getString(findViewById<null>(R.id.password))
-        FirebaseAuth.getInstance().signInWithEmailAndPassword(
-            email, password
-        ).addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Toast.makeText(this, "Login Succesfully", Toast.LENGTH_SHORT).show()
-            } else {
-                Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+        login.setOnClickListener {
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email.text.toString(),password.text.toString()
+            ).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "Login Succesfully", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
+
 }
