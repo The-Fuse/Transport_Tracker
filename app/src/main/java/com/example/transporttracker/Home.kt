c77ee3fb894e97b7872367aeac8330afd102d551
rohit141002 <rohitkumaryadav141002@gmail.com>
2020-04-09 22:51:10 +0530

    Added error for login page for empty value passing.

--- app/src/main/java/com/example/transporttracker/Home.kt
+++ app/src/main/java/com/example/transporttracker/Home.kt
@@ -3,12 +3,12 @@ package com.example.transporttracker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_home2.*
+import kotlinx.android.synthetic.main.activity_home2.email
+import kotlinx.android.synthetic.main.activity_home2.password
 
 class Home : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,21 +19,36 @@ class Home : AppCompatActivity() {
             startActivity(intent)
         }
         login.setOnClickListener {
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(
-                email.text.toString(),password.text.toString()
-            ).addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(this, "Login Succesfully", Toast.LENGTH_SHORT).show()
-                    val intent = Intent(this,MainActivity::class.java)
-                    intent.flags=
-                        Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    startActivity(intent)
-                    finish()
-                } else {
-                    Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
-                }
-            }
+
         }
+    }
+    private fun logintofirebase(){
+        val etemail: EditText = findViewById(R.id.email)
+        val etpassword:EditText= findViewById(R.id.password)
+        val email=email.text.toString()
+        val password= password.text.toString()
+        if (email.isEmpty()){
+            etemail.error="Required"
+        }else if(password.isEmpty()){
+            etpassword.error="Required"
+        }else {
+
+           FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+               .addOnCompleteListener { task ->
+                   if (task.isSuccessful) {
+                       Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show()
+                       val intent = Intent(this, MainActivity::class.java)
+                       intent.flags =
+                           Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                       startActivity(intent)
+                       finish()
+                   } else {
+                       Toast.makeText(this, "Login failed", Toast.LENGTH_SHORT).show()
+                   }
+            }
+
+        }
+
     }
 
 }
